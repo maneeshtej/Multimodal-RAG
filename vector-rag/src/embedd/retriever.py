69a637a716f044e7e3398ead47dc2e87ff73ae1a
apple <apple@apples-MacBook-Air.local>
2025-12-11 08:32:41 +0530

    added sql-rag and vector-rag to super repo

--- vector-rag/src/embedd/retriever.py
+++
@@ -1,9 +0,0 @@
-class Retriever:
-    def __init__(self, embedder, store, top_k=5):
-        self.embedder = embedder
-        self.store = store
-        self.top_k = top_k
-
-    def search(self, query):
-        emb = self.embedder.encode([query])[0]
-        return self.store.query(emb, top_k=self.top_k)
